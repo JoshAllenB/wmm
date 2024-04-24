e446f7e7abe20c95f686d75b5823c125610e30f6
Joshua Allen <batayolajoshuaallen@gmail.com>
2024-04-24 18:56:16 +0800

    topbar ui

---
+++ client/src/components/UI/Topbar.jsx
@@ -0,0 +1,46 @@
+import { useContext } from "react";
+import { IconButton, useTheme } from "@mui/material";
+import { ColorModeContext, tokens } from "./Theme/theme.utils";
+import AccountCircleSharpIcon from "@mui/icons-material/AccountCircleSharp";
+import SettingsSharpIcon from "@mui/icons-material/SettingsSharp";
+import NotificationsActiveSharpIcon from "@mui/icons-material/NotificationsActiveSharp";
+import LightModeSharpIcon from "@mui/icons-material/LightModeSharp";
+import DarkModeSharpIcon from "@mui/icons-material/DarkModeSharp";
+
+export default function Topbar() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  const handleClick = () => {
+    console.log("icon clicked...");
+    colorMode.toggleColorMode();
+  };
+
+  console.log("Current theme mode:", theme.palette.mode);
+
+  return (
+    <div
+      style={{ backgroundColor: colors.neutral }}
+      className="w-full p-4 flex justify-end"
+    >
+      <IconButton onClick={handleClick} className="w-[50px]">
+        {theme.palette.mode === "dark" ? (
+          <LightModeSharpIcon color="neutral" />
+        ) : (
+          <DarkModeSharpIcon color="neutral" />
+        )}
+      </IconButton>
+
+      <IconButton className="w-[50px]">
+        <NotificationsActiveSharpIcon color="neutral" />
+      </IconButton>
+      <IconButton className="w-[50px]">
+        <SettingsSharpIcon color="neutral" />
+      </IconButton>
+      <IconButton className="w-[50px]">
+        <AccountCircleSharpIcon color="neutral" />
+      </IconButton>
+    </div>
+  );
+}
