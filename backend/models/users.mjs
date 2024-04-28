a2984c3c1a622ab657880e89128e34340a7df641
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-04-28 19:56:12 +0800

    new schema for authentication, added password hash and slat

--- backend/models/users.mjs
+++ backend/models/users.mjs
@@ -1,5 +1,7 @@
 // user.mjs
 import mongoose from "mongoose";
+import { v4 as uuidv4 } from "uuid";
+import crypto from "crypto"; // Import the crypto module
 
 // Create a new connection for the "wmm_user" database
 const userConnection = mongoose.createConnection(
@@ -8,13 +10,50 @@ const userConnection = mongoose.createConnection(
 
 const UsersSchema = new mongoose.Schema(
   {
-    username: String,
-    password: String,
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    encrypted_password: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+    lastLoginAt: Date,
+    loggedIn: Boolean,
   },
-  {
-    versionKey: false,
-  }
+  { timestamps: true }
 );
+
+UsersSchema.virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv4();
+    this.encrypted_password = this.securePassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+UsersSchema.methods = {
+  authenticate(plainpassword) {
+    return this.securePassword(plainpassword) === this.encrypted_password;
+  },
+
+  securePassword(plainpassword) {
+    if (!plainpassword) return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainpassword)
+        .digest("hex");
+    } catch (err) {
+      return "error in hashing the password";
+    }
+  },
+};
 
 const UserModel = userConnection.model("users", UsersSchema);
 
