8dcb1f101b0f9ee2018a8d24f5fa937e1c2fecd5
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-04-28 20:06:14 +0800

    updated styling and route for userAuth

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import bcrypt from "bcryptjs";
-import { Tabs } from "@/components/ui/tabs";
-import "../styles/userAuth.css";
-import { Button } from "./ui/button";
+import { Tabs, TabsContent } from "./UI/ShadCN/tabs";
+import { Button } from "./UI/ShadCN/button";
 
 const RegisterPage = () => {
   const [username, setUsername] = useState("");
@@ -33,13 +31,12 @@ const RegisterPage = () => {
     }
 
     try {
-      const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
-      const result = await axios.post("http://localhost:3001/register", {
+      const result = await axios.post("http://localhost:3001/auth/register", {
         username,
-        password: hashedPassword, // Send hashed password to the server
+        password,
       });
 
-      navigate("/login");
+      navigate("/");
 
       console.log(result);
     } catch (err) {
@@ -48,34 +45,46 @@ const RegisterPage = () => {
   };
 
   return (
-    <Tabs defaultValue="account" className="w-[400px]">
-      <form onSubmit={handleSubmit}>
-        <div className="username">
-          <label>Username:</label>
-          <input type="text" value={username} onChange={handleUsernameChange} />
-        </div>
-        <div className="password">
-          <label htmlFor="password">Password:</label>
+    <Tabs
+      defaultValue="account"
+      className="flex min-h-full  flex-1 flex-col justify-center items-center "
+    >
+      <TabsContent value="account">
+        <form onSubmit={handleSubmit} className="space-y-3 w-[350px]">
+          <label className="block text-sm font-medium leading-6 text-gray-200">
+            Username:
+          </label>
+          <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-gray-300 placeholder:text-gray-300 focus:ring-3 p-3"
+          />
+          <label className="block text-sm font-medium leading-6 text-gray-200">
+            Password:
+          </label>
           <input
             type="password"
-            id="password"
             value={password}
             onChange={handlePasswordChange}
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-gray-300 placeholder:text-gray-300 focus:ring-3 p-3"
           />
-          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <label className="block text-sm font-medium leading-6 text-gray-200">
+            Confirm Password:
+          </label>
           <input
             type="password"
-            id="confirm-password"
             value={confirmPassword}
             onChange={handleConfirmPasswordChange}
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-gray-300 placeholder:text-gray-300 focus:ring-3 p-3"
           />
-        </div>
-        {errorMessage && <p className="error-message">{errorMessage}</p>}
-        <Button type="submit">Register</Button>
-      </form>
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
+          <Button type="submit" className="bg-gray-700 border">
+            Register
+          </Button>
+        </form>
+      </TabsContent>
     </Tabs>
-    // < className="register-container">
-    //   <h2>Register</h2>
   );
 };
 
