152393b953fbea7d3ec96efa4eaa6ab191ac0385
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-05-07 15:08:38 +0800

    added metadata for auditing

--- backend/models/clients.mjs
+++ backend/models/clients.mjs
@@ -9,27 +9,21 @@ const clientConnection = mongoose.createConnection(
 const ClientSchema = new mongoose.Schema(
   {
     id: Number,
-    Name: {
-      lname: String,
-      fname: String,
-      mname: String,
-      sname: String,
-      title: String,
-    },
+    lname: String,
+    fname: String,
+    mname: String,
+    sname: String,
+    title: String,
     bdate: String,
     company: String,
-    Address: {
-      address: String,
-      zipcode: Number,
-      area: String,
-      acode: String,
-    },
-    ContactInfo: {
-      contactnos: String,
-      cellno: String,
-      ofcno: String,
-      email: String,
-    },
+    address: String,
+    zipcode: Number,
+    area: String,
+    acode: String,
+    contactnos: String,
+    cellno: String,
+    ofcno: String,
+    email: String,
     type: String,
     group: String,
     remarks: String,
@@ -39,17 +33,17 @@ const ClientSchema = new mongoose.Schema(
     subscriptionStart: String,
     subscriptionEnd: String,
     copies: Number,
+    metadata: {
+      addedBy: String,
+      addedAt: { type: Date, default: Date.now },
+      editedBy: String,
+      editedAt: { type: Date },
+    },
   },
   {
     versionKey: false,
   }
 );
-
-ClientSchema.virtual("fullName").get(function () {
-  return `${this.title} ${this.lname} ${this.fname} ${this.mname} ${this.sname}`;
-});
-
-ClientSchema.index({ fullName: "text" });
 
 const ClientModel = clientConnection.model("clients", ClientSchema);
 
