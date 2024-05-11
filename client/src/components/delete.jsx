22f99b69a2d2aeaebdba3f281f0c8e01367b10c1
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-05-11 13:56:33 +0800

    updated delete logic

--- client/src/components/delete.jsx
+++ client/src/components/delete.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
-import { Button } from "./ui/button";
+import { Button } from "./UI/ShadCN/button";
 import Modal from "./modal";
+import axios from "axios";
 
 const Delete = ({ client, onDelete, onClose }) => {
   const [showConfirmation, setShowConfirmation] = useState(false);
 
   const handleDelete = async () => {
     try {
-      await onDelete(client._id);
-      onClose(); // Close the modal after successful deletion
-    } catch (error) {
-      console.error("Error deleting client:", error);
-      // Optionally, you can show a message to the user indicating the deletion failed
+      await axios.delete(`http://localhost:3001/clients/${client.id}`);
+      console.log("Client deleted successfully:", client.id);
+      if (onDelete) {
+        onDelete(client.id);
+      }
+      onClose();
+    } catch (e) {
+      console.error("Error deleting client", e);
     }
   };
 
@@ -25,11 +29,13 @@ const Delete = ({ client, onDelete, onClose }) => {
       </Button>
       {showConfirmation && (
         <Modal onClose={() => setShowConfirmation(false)}>
-          <h2 className="text-xl font-bold mb-4">Delete Client</h2>
-          <p>Are you sure you want to delete this client?</p>
+          <h2 className="text-xl font-bold mb-4 text-black">Delete Client</h2>
+          <p className="text-black">
+            Are you sure you want to delete this client?
+          </p>
           <div className="flex justify-start mt-4 gap-1">
             <Button
-              onClick={handleDelete}
+              onClick={() => handleDelete(client.id)}
               className="ml-2 bg-red-500 hover:bg-red-900"
             >
               Delete
