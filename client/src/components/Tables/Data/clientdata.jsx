c6890359f81494e6ac0ad7aa43c8e4889c28b023
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-06-10 16:33:35 +0800

    minor changes in the column style

--- client/src/components/Tables/Data/clientdata.jsx
+++ client/src/components/Tables/Data/clientdata.jsx
@@ -24,54 +24,98 @@ export const columns = [
   {
     id: "select",
     header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
+      <div>
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => {
+            table.toggleAllPageRowsSelected(!!value);
+          }}
+          aria-label="Select all"
+        />
+      </div>
     ),
     cell: ({ row }) => (
       <Checkbox
         checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        onCheckedChange={(value) => {
+          row.toggleSelected(!!value);
+        }}
         aria-label="Select row"
       />
     ),
     enableSorting: false,
     enableHiding: false,
+    size: 20, // Example size
   },
-  { id: "ID", Header: "ID", accessorFn: (row) => row.id },
+  { id: "ID", Header: "ID", accessorFn: (row) => row.id, size: 50 },
   {
     id: "Name",
     Header: "Name",
     accessorFn: (row) =>
       `${row.sname} ${row.lname} ${row.fname} ${row.mname} ${row.title}`,
+    size: 150,
   },
-  { id: "Birth Date", Header: "Birth Date", accessorFn: (row) => row.bdate },
-  { id: "Company", Header: "Company", accessorFn: (row) => row.company },
-  { id: "Address", Header: "Address", accessorFn: (row) => row.address },
-  { id: "Zipcode", Header: "Zipcode", accessorFn: (row) => row.zipcode },
-  { id: "Area", Header: "Area", accessorFn: (row) => row.area },
-  { id: "Area Code", Header: "Area Code", accessorFn: (row) => row.acode },
+  {
+    id: "Birth Date",
+    Header: "Birth Date",
+    accessorFn: (row) => row.bdate,
+    size: 300,
+  },
+  {
+    id: "Company",
+    Header: "Company",
+    accessorFn: (row) => row.company,
+    size: 100,
+  },
+  {
+    id: "Address",
+    Header: "Address",
+    accessorFn: (row) => {
+      const { street, city, barangay, address } = row;
+      const concatAddress = `${street || ""} ${city || ""} ${
+        barangay || ""
+      }`.trim();
+      return concatAddress || address;
+    },
+    size: 1500,
+  },
+  {
+    id: "Zipcode",
+    Header: "Zipcode",
+    accessorFn: (row) => row.zipcode,
+    size: 50,
+  },
+  { id: "Area", Header: "Area", accessorFn: (row) => row.area, size: 100 },
+  {
+    id: "Area Code",
+    Header: "Area Code",
+    accessorFn: (row) => row.acode,
+    size: 100,
+  },
   {
     id: "Contact Information",
     Header: "Contact Info",
     accessorFn: (row) => `${row.contactnos} ${row.cellno} ${row.ofcno}`,
+    size: 500,
   },
-  { id: "Type", Header: "Type", accessorFn: (row) => row.type },
-  { id: "Group", Header: "Group", accessorFn: (row) => row.group },
-  { id: "Remarks", Header: "Remarks", accessorFn: (row) => row.remarks },
+  { id: "Type", Header: "Type", accessorFn: (row) => row.type, size: 100 },
+  { id: "Group", Header: "Group", accessorFn: (row) => row.group, size: 100 },
+  {
+    id: "Remarks",
+    Header: "Remarks",
+    accessorFn: (row) => row.remarks,
+    enableResizing: true,
+    size: 300, // Adjust the size as needed
+  },
   {
     id: "Subscription",
     Header: "Subscription",
     accessorFn: (row) => {
       const { subscriptionFreq, subscriptionStart, subscriptionEnd, copies } =
         row;
-
-      // Check if all data is missing
       if (
         !subscriptionFreq &&
         !subscriptionStart &&
@@ -80,8 +124,6 @@ export const columns = [
       ) {
         return "";
       }
-
-      // Check if any data is missing and return empty string if true
       if (
         !subscriptionFreq ||
         !subscriptionStart ||
@@ -90,9 +132,8 @@ export const columns = [
       ) {
         return "";
       }
-
-      // Return formatted string if all data is present
       return `${subscriptionFreq} Months: ${subscriptionStart} ${subscriptionEnd} Copies: ${copies}`;
     },
+    size: 250,
   },
 ];
