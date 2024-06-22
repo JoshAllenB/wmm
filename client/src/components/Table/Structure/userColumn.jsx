05bf0cbfe993c11b26b7a1d8ca9694bc3147cb69
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-06-22 19:21:31 +0800

    user column structure with simple UI for status

---
+++ client/src/components/Table/Structure/userColumn.jsx
@@ -0,0 +1,46 @@
+/** @type import('@tanstack/react-table').ColumnDef<any>[] */
+export const userColumns = [
+  {
+    id: "status",
+    header: "Status",
+    accessorFn: (row) => row.status,
+    cell: ({ getValue }) => {
+      const status = getValue();
+      let bgColor;
+      switch (status) {
+        case "Active":
+          bgColor = "bg-green-500";
+          break;
+        case "Inactive":
+          bgColor = "bg-yellow-500";
+          break;
+        case "Logged Off":
+          bgColor = "bg-red-500";
+          break;
+        default:
+          bgColor = "bg-gray-500";
+      }
+      return (
+        <span className={`px-2 py-1 rounded-full ${bgColor} text-white`}>
+          {status}
+        </span>
+      );
+    },
+  },
+  {
+    id: "username",
+    header: "Username",
+    accessorFn: (row) => row.username,
+  },
+  {
+    id: "role",
+    header: "Role",
+    accessorFn: (row) => row.role,
+  },
+  {
+    id: "lastLoginAt",
+    header: "Last Login",
+    accessorFn: (row) =>
+      row.lastLoginAt ? new Date(row.lastLoginAt).toLocaleString() : "Never",
+  },
+];
