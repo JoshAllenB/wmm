d0d757087f0b890c91770030283b9e195e5826ae
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-06-22 19:07:22 +0800

    added websocket functionality for user status

--- backend/userAuth/login.mjs
+++ backend/userAuth/login.mjs
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import http from "http";
+import { Server } from "socket.io";
 import UserModel from "../models/users.mjs";
 
 const generateToken = (userId) => {
@@ -7,21 +9,48 @@ const generateToken = (userId) => {
   });
 };
 
+const server = http.createServer();
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", // Allow your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  },
+});
+
 const loginUser = async (username, password) => {
+  
   try {
     const user = await UserModel.findOne({ username });
 
     if (!user) {
-      return { error: "Username does not exist" };
+      return { error: "Invalid username or password" };
     }
 
     if (!user.authenticate(password)) {
-      return { error: "Password Incorrect" };
+      return { error: "Invalid username or password" };
     }
+
+    user.status.status = "Active";
+    user.lastLoginAt = new Date();
+    await user.save();
+
+    io.emit("user_status_change", { userId: user._id, status: "Active" });
 
     const token = generateToken(user._id);
 
-    return { token };
+    return {
+      token,
+      user: {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+        status: user.status.status,
+        lastLoginAt: user.lastLoginAt,
+      },
+    };
   } catch (err) {
     console.error(err);
     return { error: "Internal Server Error" };
