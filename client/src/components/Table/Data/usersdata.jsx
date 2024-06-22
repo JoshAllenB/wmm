fdf34a118a409284b9a0b941e86a28bd12e5b101
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-06-22 19:37:35 +0800

    removed clg and simplified the return code

--- client/src/components/Table/Data/usersdata.jsx
+++ client/src/components/Table/Data/usersdata.jsx
@@ -3,12 +3,10 @@ import axios from "axios";
 export const fetchUsers = async () => {
   try {
     const response = await axios.get("http://localhost:3001/users");
-    const formattedData = response.data.map((user) => ({
+    return response.data.map((user) => ({
       ...user,
       status: user.status.status || user.status, // Flatten the nested status field
     }));
-    console.log("Formatted user data:", formattedData); // Add this log
-    return formattedData;
   } catch (e) {
     console.error("Error fetching user data:", e);
     throw e;
