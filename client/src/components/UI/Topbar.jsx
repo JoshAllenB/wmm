e5b6708758c289ddb741b73d2638b86bff2cd303
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-06-22 19:23:53 +0800

    added dropdown menu for the account icon and logout logic

--- client/src/components/UI/Topbar.jsx
+++ client/src/components/UI/Topbar.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { IconButton, useTheme } from "@mui/material";
 import { ColorModeContext, tokens } from "./Theme/theme.utils";
 import AccountCircleSharpIcon from "@mui/icons-material/AccountCircleSharp";
@@ -6,18 +6,69 @@ import SettingsSharpIcon from "@mui/icons-material/SettingsSharp";
 import NotificationsActiveSharpIcon from "@mui/icons-material/NotificationsActiveSharp";
 import LightModeSharpIcon from "@mui/icons-material/LightModeSharp";
 import DarkModeSharpIcon from "@mui/icons-material/DarkModeSharp";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ShadCN/components/ui/dropdown-menu";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import setAuthToken from "../../utils/setAuthToken";
 
-export default function Topbar() {
+import io from "socket.io-client";
+const socket = io("http://localhost:3001");
+
+export default function Topbar({ setIsLoggedIn }) {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
+  const navigate = useNavigate();
+  const [position, setPosition] = useState("bottom");
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setAuthToken(token);
+    }
+  }, []);
 
   const handleClick = () => {
-    console.log("icon clicked...");
     colorMode.toggleColorMode();
   };
 
-  console.log("Current theme mode:", theme.palette.mode);
+  const handleLogout = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      setAuthToken();
+
+      const response = await axios.post(
+        "http://localhost:3001/auth/logout",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.data.message === "Logout successful") {
+        localStorage.removeItem("token");
+        setAuthToken(null);
+        setIsLoggedIn(false);
+        socket.emit("user_status_change", {
+          userId: response.data.userId,
+          status: "Logged Off",
+        });
+        navigate("/");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div
@@ -38,9 +89,22 @@ export default function Topbar() {
       <IconButton className="w-[50px]">
         <SettingsSharpIcon color="neutral" />
       </IconButton>
-      <IconButton className="w-[50px]">
-        <AccountCircleSharpIcon color="neutral" />
-      </IconButton>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <IconButton className="w-[50px]">
+            <AccountCircleSharpIcon color="neutral" />
+          </IconButton>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>Account Settings</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+            <DropdownMenuRadioItem onClick={handleLogout}>
+              Log-Out
+            </DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
