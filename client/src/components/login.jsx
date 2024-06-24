8de3e2c09ec4a1c8512f50e4a409fbd4aa5f5c12
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-06-24 20:59:41 +0800

    added token validation login

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "./UI/ShadCN/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./UI/ShadCN/tabs";
 import RegisterPage from "./register";
 import setAuthToken from "../utils/setAuthToken";
+import validateToken from "../utils/validateToken";
 import io from "socket.io-client";
+import { setToken } from "../utils/tokenStorage";
 
 const socket = io("http://localhost:3001");
 
@@ -13,7 +15,20 @@ const LoginPage = ({ setIsLoggedIn }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [activeTab, setActiveTab] = useState("account");
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const user = await validateToken();
+      if (user) {
+        setIsLoggedIn(true);
+        navigate("/all-client");
+      }
+    };
+
+    checkAuth();
+  }, []);
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
@@ -21,6 +36,16 @@ const LoginPage = ({ setIsLoggedIn }) => {
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
+  };
+
+  const handleTabChange = (value) => {
+    setActiveTab(value);
+  };
+
+  const handleRegisterSuccess = () => {
+    setTimeout(() => {
+      setActiveTab("account");
+    }, 200);
   };
 
   const handleSubmit = async (e) => {
@@ -33,7 +58,7 @@ const LoginPage = ({ setIsLoggedIn }) => {
       });
 
       if (result.data.token) {
-        localStorage.setItem("token", result.data.token);
+        setToken(result.data.token);
         setAuthToken(result.data.token);
 
         setIsLoggedIn(true);
@@ -43,7 +68,6 @@ const LoginPage = ({ setIsLoggedIn }) => {
           userId: result.data.user._id,
           status: "Active",
         });
-
       } else {
         setErrorMessage(result.data.error);
       }
@@ -60,17 +84,29 @@ const LoginPage = ({ setIsLoggedIn }) => {
   };
 
   return (
-    <div className="flex min-h-full  flex-1 flex-col justify-center items-center ">
-      <Tabs defaultValue="account" className="w-[350px] h-[300px]">
-        <TabsList className="flex gap-[80px] ">
-          <TabsTrigger value="account" className="font-roboto text-xl">
+    <div className="flex min-h-full flex-1 flex-col justify-center items-center ">
+      <Tabs
+        value={activeTab}
+        onValueChange={setActiveTab}
+        className="w-[400px] h-[400px] bg-white-700 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-60 border border-gray-100 "
+      >
+        <TabsList className="flex gap-[80px] p-7 rounded-md">
+          <TabsTrigger
+            value="account"
+            className="font-roboto text-xl py-1 rounded-md"
+            onClick={() => handleTabChange("account")}
+          >
             Login
           </TabsTrigger>
-          <TabsTrigger value="register" className="font-roboto text-xl">
+          <TabsTrigger
+            value="register"
+            className="font-roboto text-xl  py-1 rounded-md"
+            onClick={() => handleTabChange("register")}
+          >
             Register
           </TabsTrigger>
         </TabsList>
-        <TabsContent value="account" className="mt-2">
+        <TabsContent value="account" className="m-10 ">
           <form onSubmit={handleSubmit} className="space-y-8">
             <div className="username">
               <label className="block text-sm font-medium leading-6 text-gray-200">
@@ -108,7 +144,7 @@ const LoginPage = ({ setIsLoggedIn }) => {
           </form>
         </TabsContent>
         <TabsContent value="register">
-          <RegisterPage />
+          <RegisterPage onSuccess={handleRegisterSuccess} />
         </TabsContent>
       </Tabs>
     </div>
