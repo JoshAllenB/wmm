dedbac43788751b4c047f8040f97683edf7b32e4
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-06-25 16:56:10 +0800

    re-organized the directory moved the crud components for all client

---
+++ client/src/components/CRUD/AllClient/edit.jsx
@@ -0,0 +1,422 @@
+import { useEffect, useMemo, useState } from "react";
+import { Button } from "../../UI/ShadCN/button";
+import Modal from "../../modal";
+import Delete from "./delete";
+import Mailing from "../../mailing";
+import InputField from "../input";
+import axios from "axios";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3001");
+
+const Edit = ({ rowData, onDelete, onClose }) => {
+  const initialFormData = useMemo(
+    () => ({
+      lname: "",
+      fname: "",
+      mname: "",
+      sname: "",
+      title: "",
+      bdate: "",
+      company: "",
+      street: "",
+      city: "",
+      barangay: "",
+      zipcode: "",
+      area: "",
+      acode: "",
+      contactnos: "",
+      cellno: "",
+      ofcno: "",
+      email: "",
+      type: "",
+      group: "",
+      remarks: "",
+      subscriptionFreq: "",
+      subscriptionStart: "",
+      subscriptionEnd: "",
+      copies: "",
+    }),
+    []
+  );
+
+  useEffect(() => {
+    const socket = io("http://localhost:3001");
+
+    socket.on("connect", () => {
+      console.log("Connected");
+    });
+
+    socket.on("connect_error", (error) => {
+      console.error("Connection Error:", error);
+    });
+
+    socket.on("data-update", (data) => {
+      console.log("Data Update:", data);
+    });
+  }, []);
+
+  const [formData, setFormData] = useState(initialFormData);
+  const [showModal, setShowModal] = useState(false);
+
+  const formatDate = (date) => {
+    const options = { month: "long", year: "numeric" };
+    return new Intl.DateTimeFormat("en-US", options).format(date);
+  };
+
+  useEffect(
+    (client) => {
+      setFormData({
+        ...initialFormData,
+        ...rowData,
+      });
+
+      socket.emit("data-update", {
+        type: "update",
+        data: client,
+      });
+      setShowModal(true);
+    },
+    [rowData, initialFormData]
+  );
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "subscriptionFreq") {
+      const today = new Date();
+      const monthsToAdd = parseInt(value);
+
+      const subscriptionStart = new Date(today);
+
+      const subscriptionEnd = new Date(subscriptionStart);
+      subscriptionEnd.setMonth(subscriptionEnd.getMonth() + monthsToAdd);
+
+      setFormData((prevData) => ({
+        ...prevData,
+        subscriptionFreq: value,
+        subscriptionStart: formatDate(subscriptionStart),
+        subscriptionEnd: formatDate(subscriptionEnd),
+      }));
+      return;
+    }
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value || "",
+    }));
+  };
+
+  useEffect(() => {}, [formData]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData) return;
+
+    try {
+      await axios.put(`http://localhost:3001/clients/${rowData.id}`, formData);
+      onClose();
+      setShowModal(false);
+    } catch (e) {
+      console.error("Error updating client:", e);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      await onDelete(rowData.id);
+      onClose();
+    } catch (e) {
+      console.error("Error deleting client:", e);
+    }
+  };
+
+  return (
+    <>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="text-xl font-bold">Edit Client Information</h2>
+            <button
+              onClick={() => setShowModal(false)}
+              className="text-gray-500 hover:text-gray-700"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="grid grid-cols-2 gap-4">
+              <div className="flex flex-col mb-2 p-2">
+                <h1 className="text-black mb-2 font-bold">Personal Info</h1>
+                <InputField
+                  label="Last Name:"
+                  id="lname"
+                  name="lname"
+                  value={formData.lname}
+                  onChange={handleChange}
+                />
+
+                <InputField
+                  label="First Name:"
+                  id="fname"
+                  name="fname"
+                  value={formData.fname}
+                  onChange={handleChange}
+                />
+
+                <InputField
+                  label="Middle Name:"
+                  id="mname"
+                  name="mname"
+                  value={formData.mname}
+                  onChange={handleChange}
+                />
+
+                <InputField
+                  label="Suffix:"
+                  id="sname"
+                  name="sname"
+                  value={formData.sname}
+                  onChange={handleChange}
+                />
+                <InputField
+                  label="Title:"
+                  id="title"
+                  name="title"
+                  value={formData.title}
+                  onChange={handleChange}
+                />
+
+                <InputField
+                  label="Birth Date:"
+                  id="bdate"
+                  name="bdate"
+                  value={formData.bdate}
+                  onChange={handleChange}
+                />
+
+                <InputField
+                  label="Company:"
+                  id="company"
+                  name="company"
+                  value={formData.company}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="flex flex-col mb-2 p-2">
+                <h1 className="text-black mb-2 font-bold">Address Info</h1>
+
+                <InputField
+                  label="Street:"
+                  id="street"
+                  name="street"
+                  value={formData.street}
+                  onChange={handleChange}
+                />
+                <InputField
+                  label="Barangay:"
+                  id="barangay"
+                  name="barangay"
+                  value={formData.barangay}
+                  onChange={handleChange}
+                />
+                <InputField
+                  label="City:"
+                  id="city"
+                  name="city"
+                  value={formData.city}
+                  onChange={handleChange}
+                />
+                <InputField
+                  label="Zip Code:"
+                  id="zipcode"
+                  name="zipcode"
+                  value={formData.zipcode}
+                  onChange={handleChange}
+                />
+                <InputField
+                  label="Area:"
+                  id="area"
+                  name="area"
+                  value={formData.area}
+                  onChange={handleChange}
+                />
+                <InputField
+                  label="Area Code:"
+                  id="acode"
+                  name="acode"
+                  value={formData.acode}
+                  onChange={handleChange}
+                />
+
+                {formData.address && (
+                  <div className="flex flex-col mb-2 p-2">
+                    <h1 className="text-black ">Old Address Format</h1>
+                    <textarea
+                      id="address"
+                      name="address"
+                      value={formData.address}
+                      onChange={handleChange}
+                      className="block w-full rounded-md border-0 mb-2 py-1.5 text-gray-900 shadow-sm ring-2 ring-gray-300 placeholder:text-gray-300 focus:ring-3 p-3"
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="flex flex-col mb-2 p-2">
+                <h1 className="text-black mb-2 font-bold">Contact Info</h1>
+                <InputField
+                  label="Contact Numbers:"
+                  id="contactnos"
+                  name="contactnos"
+                  value={formData.contactnos}
+                  onChange={handleChange}
+                />
+
+                <InputField
+                  label="Cell Number:"
+                  id="cellno"
+                  name="cellno"
+                  value={formData.cellno}
+                  onChange={handleChange}
+                />
+
+                <InputField
+                  label="Office Number:"
+                  id="ofcno"
+                  name="ofcno"
+                  value={formData.ofcno}
+                  onChange={handleChange}
+                />
+
+                <InputField
+                  label="Email:"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex flex-col mb-2 p-2">
+                <h1 className="text-black mb-2 font-bold">Group Info</h1>
+                <InputField
+                  label="Type:"
+                  id="type"
+                  name="type"
+                  value={formData.type}
+                  onChange={handleChange}
+                />
+
+                <InputField
+                  label="Group:"
+                  id="group"
+                  name="group"
+                  value={formData.group}
+                  onChange={handleChange}
+                />
+
+                <InputField
+                  label="Remarks:"
+                  id="remarks"
+                  name="remarks"
+                  value={formData.remarks}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex flex-col mb-2">
+                <h1 className="text-black mb-2 font-bold">Subscription</h1>
+                <label htmlFor="subcriptionFreq">Subscription Frequency:</label>
+                <select
+                  id="subscriptionFreq"
+                  name="subscriptionFreq"
+                  value={formData.subscriptionFreq}
+                  onChange={handleChange}
+                  className="block w-full rounded-md border-0 mb-2 py-1.5 text-gray-900 shadow-sm ring-2 ring-gray-300 placeholder:text-gray-300 focus:ring-3 p-3"
+                >
+                  <option value="">Select Frequency</option>
+                  <option value="3">3 Months</option>
+                  <option value="6">6 Months</option>
+                  <option value="12">12 Months</option>
+                </select>
+
+                <InputField
+                  label="Subscription Start:"
+                  id="subscriptionStart"
+                  name="subscriptionStart"
+                  value={formData.subscriptionStart}
+                  onChange={handleChange}
+                />
+
+                <InputField
+                  label="Subscription End:"
+                  id="subscriptionEnd"
+                  name="subscriptionEnd"
+                  value={formData.subscriptionEnd}
+                  onChange={handleChange}
+                />
+
+                <label className="block text-sm font-medium leading-6 text-gray-600">
+                  Copies:
+                </label>
+                <input
+                  id="copies"
+                  name="copies"
+                  value={formData.copies || ""}
+                  onChange={handleChange}
+                  type="number"
+                  className="block w-[80px] rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-gray-300 placeholder:text-gray-300 focus:ring-3 p-3"
+                />
+              </div>
+            </div>
+          </form>
+          <div className="mt-4 flex justify-between">
+            <div className="flex gap-1">
+              <Button
+                onClick={handleSubmit}
+                className="text-sm bg-green-600 hover:bg-green-800 "
+              >
+                Save
+              </Button>
+              <Button onClick={onClose} className="bg-red-500 hover:bg-red-800">
+                Cancel
+              </Button>
+            </div>
+            <div className="flex gap-1">
+              <Mailing
+                id={formData.id}
+                address={formData.address}
+                areaCode={formData.acode}
+                zipcode={formData.zipcode}
+                lname={formData.lname}
+                fname={formData.fname}
+                mname={formData.mname}
+                contactnos={formData.contactnos}
+                cellno={formData.cellno}
+                officeno={formData.ofcno}
+              />
+
+              <Delete
+                client={rowData}
+                onDelete={handleDelete}
+                onClose={onClose}
+              />
+            </div>
+          </div>
+        </Modal>
+      )}
+    </>
+  );
+};
+
+export default Edit;
