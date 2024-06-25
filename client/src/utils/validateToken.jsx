d3ba54270392ce9ca11e4a48de1bcc5930ae2858
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-06-25 17:53:58 +0800

    fixed typo and simplified the code

--- client/src/utils/validateToken.jsx
+++ client/src/utils/validateToken.jsx
@@ -19,26 +19,27 @@ const validateToken = async () => {
     }
   } catch (error) {
     if (error.response && error.response.status === 401) {
-      
-      try {
-        const refreshReponse = await axios.post(
-          "http://localhost:3001/auth/refreshToken",
-          { token }
-        );
-        if (refreshReponse.data.token) {
-          setToken(refreshReponse.data.token);
-          setAuthToken(refreshReponse.data.token);
-
-          const newValidationReponse = await axios.post(
-            "http://localhost:3001/auth/verifyToken",
-            { token: refreshReponse.data.token }
+      if (getToken()) {
+        try {
+          const refreshResponse = await axios.post(
+            "http://localhost:3001/auth/refreshToken",
+            { token }
           );
-          if (newValidationReponse.data.valid) {
-            return newValidationReponse.data.user;
+          if (refreshResponse.data.token) {
+            setToken(refreshResponse.data.token);
+            setAuthToken(refreshResponse.data.token);
+
+            const newValidResponse = await axios.post(
+              "http://localhost:3001/auth/verifyToken",
+              { token: refreshResponse.data.token }
+            );
+            if (newValidResponse.data.valid) {
+              return newValidResponse.data.user;
+            }
           }
+        } catch (refreshError) {
+          removeToken();
         }
-      } catch (refreshError) {
-        removeToken();
       }
     } else {
       console.error("Token validation error:", error);
