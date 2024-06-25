7c79810383f0876d4e5432bf8ed8ce9a32d5f93d
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-06-25 17:49:25 +0800

    removed logout logic

--- client/src/components/UI/Topbar.jsx
+++ client/src/components/UI/Topbar.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext } from "react";
 import { IconButton, useTheme } from "@mui/material";
 import { ColorModeContext, tokens } from "./Theme/theme.utils";
 import AccountCircleSharpIcon from "@mui/icons-material/AccountCircleSharp";
@@ -10,64 +10,18 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuLabel,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ShadCN/dropdown-menu";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import setAuthToken from "../../utils/setAuthToken";
-
-import io from "socket.io-client";
-const socket = io("http://localhost:3001");
+import Logout from "../../utils/UserAuth/logout";
 
 export default function Topbar({ setIsLoggedIn }) {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
-  const navigate = useNavigate();
-  const [position, setPosition] = useState("bottom");
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      setAuthToken(token);
-    }
-  }, []);
 
   const handleClick = () => {
     colorMode.toggleColorMode();
-  };
-
-  const handleLogout = async () => {
-    try {
-      const token = localStorage.getItem("token");
-      setAuthToken();
-
-      const response = await axios.post(
-        "http://localhost:3001/auth/logout",
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-
-      if (response.data.message === "Logout successful") {
-        localStorage.removeItem("token");
-        setAuthToken(null);
-        setIsLoggedIn(false);
-        socket.emit("user_status_change", {
-          userId: response.data.userId,
-          status: "Logged Off",
-        });
-        navigate("/");
-      }
-    } catch (error) {
-      console.error(error);
-    }
   };
 
   return (
@@ -98,11 +52,7 @@ export default function Topbar({ setIsLoggedIn }) {
         <DropdownMenuContent>
           <DropdownMenuLabel>Account Settings</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-            <DropdownMenuRadioItem onClick={handleLogout}>
-              Log-Out
-            </DropdownMenuRadioItem>
-          </DropdownMenuRadioGroup>
+          <Logout setIsLoggedIn={setIsLoggedIn} />
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
