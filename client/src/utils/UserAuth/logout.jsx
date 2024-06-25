33d4986defb15c6465cc65e305741e1f0c7c832f
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-06-25 17:50:22 +0800

    fixed bug not logging out properly || new component

---
+++ client/src/utils/UserAuth/logout.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import setAuthToken from "../setAuthToken";
+import {
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+} from "../../components/UI/ShadCN/dropdown-menu";
+import io from "socket.io-client";
+import { removeToken } from "../tokenStorage";
+
+const socket = io("http://localhost:3001");
+
+export default function Logout({ setIsLoggedIn }) {
+  const navigate = useNavigate();
+  const [position, setPosition] = useState("bottom");
+
+  const handleLogout = async () => {
+    try {
+      const token =
+        localStorage.getItem("token") || sessionStorage.getItem("token");
+      if (!token) {
+        console.error("No token found");
+        setIsLoggedIn(false);
+        navigate("/");
+        return;
+      }
+
+      const reponse = await axios.post(
+        "http://localhost:3001/auth/logout",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (reponse.data.message === "Logout successful") {
+        localStorage.removeItem("token");
+        sessionStorage.removeItem("token");
+        removeToken();
+        setAuthToken(null);
+        setIsLoggedIn(false);
+        socket.emit("user_status_change", {
+          userId: reponse.data.userId,
+          status: "Logged Off",
+        });
+        navigate("/");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+      <DropdownMenuRadioItem onClick={handleLogout}>
+        Logout
+      </DropdownMenuRadioItem>
+    </DropdownMenuRadioGroup>
+  );
+}
