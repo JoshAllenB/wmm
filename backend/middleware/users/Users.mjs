3239c38eba15511373fbb181bae0b461afcb9247
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-06-27 14:57:26 +0800

    added CRUD api routes

--- backend/middleware/users/Users.mjs
+++ backend/middleware/users/Users.mjs
@@ -3,6 +3,8 @@ import http from "http";
 import { Server } from "socket.io";
 import initWebSocket from "../../websocket.mjs";
 import UserModel from "../../models/users.mjs";
+import { isAdmin } from "./adminAuth.mjs";
+import verifyToken from "../../userAuth/verifyToken.mjs";
 
 const server = http.createServer();
 const io = new Server(server, {
@@ -17,7 +19,8 @@ const io = new Server(server, {
 const router = express.Router();
 initWebSocket(io);
 
-router.get("/", async (req, res) => {
+// Fetch all user to be used in table
+router.get("/", verifyToken, isAdmin, async (req, res) => {
   const io = req.io;
   try {
     const users = await UserModel.find().select(
@@ -29,4 +32,54 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Add user
+router.post("/add", verifyToken,  async (req, res) => {
+  try {
+    const { username, password, role } = req.body;
+    const newUser = new UserModel({ username, password, role });
+    await newUser.save();
+    res
+      .status(201)
+      .json({ message: "User created successfully", user: newUser });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Update user
+router.put("/update/:id", verifyToken, async (req, res) => {
+  try {
+    const { username, role } = req.body;
+    const updateData = { username };
+
+    if (req.user.role === "Admin") {
+      updateData.role = role;
+    }
+
+    const user = await UserModel.findByIdAndUpdate(req.params.id, updateData, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.json({ message: "User updated successfully", user });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+// Delete user
+router.delete("/delete/:id", verifyToken, isAdmin, async (req, res) => {
+  try {
+    const user = await UserModel.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.json({ message: "User deleted successfully" });
+  } catch (err) {
+    console.error(err);
+  }
+});
 export default router;
