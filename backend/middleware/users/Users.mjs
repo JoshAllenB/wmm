d675d1d3f351a7865324e236c30cf69a2ad329d6
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-07-15 17:17:39 +0800

    added socket.io for realtime update in admin panel

--- backend/middleware/users/Users.mjs
+++ backend/middleware/users/Users.mjs
@@ -27,13 +27,15 @@ router.get("/", verifyToken, isAdmin, async (req, res) => {
       "username role lastLoginAt loggedIn status"
     );
     res.status(200).json(users);
+    io.emit("user-update", { type: "init", data: users });
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch users" });
   }
 });
 
 // Add user
-router.post("/add", verifyToken,  async (req, res) => {
+router.post("/add", verifyToken, async (req, res) => {
+  const io = req.io;
   try {
     const { username, password, role } = req.body;
     const newUser = new UserModel({ username, password, role });
@@ -41,6 +43,7 @@ router.post("/add", verifyToken,  async (req, res) => {
     res
       .status(201)
       .json({ message: "User created successfully", user: newUser });
+    io.emit("user-update", { type: "add", data: newUser });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -48,6 +51,7 @@ router.post("/add", verifyToken,  async (req, res) => {
 
 // Update user
 router.put("/update/:id", verifyToken, async (req, res) => {
+  const io = req.io;
   try {
     const { username, role } = req.body;
     const updateData = { username };
@@ -65,19 +69,27 @@ router.put("/update/:id", verifyToken, async (req, res) => {
       return res.status(404).json({ error: "User not found" });
     }
     res.json({ message: "User updated successfully", user });
+    io.emit("user-update", {
+      type: "update",
+      data: { ...user.toObject(), id: user._id },
+    });
   } catch (err) {
     console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // Delete user
 router.delete("/delete/:id", verifyToken, isAdmin, async (req, res) => {
+  const io = req.io;
   try {
     const user = await UserModel.findByIdAndDelete(req.params.id);
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
     res.json({ message: "User deleted successfully" });
+    console.log("User deleted:", user);
+    io.emit("user-update", { type: "delete", data: user });
   } catch (err) {
     console.error(err);
   }
