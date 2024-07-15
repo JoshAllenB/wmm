00aad35601ee715f2df51bfc1d407d77caf0b638
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-07-15 17:34:16 +0800

    useReact table structure

---
+++ client/src/components/Table/TableLogic.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import {
+  useReactTable,
+  getCoreRowModel,
+  getSortedRowModel,
+  getPaginationRowModel,
+  getFilteredRowModel,
+} from "@tanstack/react-table";
+
+export function useTableLogic(data, columns, usePagination, page, pageSize) {
+  const [sorting, setSorting] = useState([]);
+  const [filtering, setFiltering] = useState("");
+  const [rowSelection, setRowSelection] = useState([]);
+
+  const tableData = Array.isArray(data) ? data : [];
+
+  return useReactTable({
+    data: tableData,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    ...(usePagination
+      ? { getPaginationRowModel: getPaginationRowModel() }
+      : {}),
+    getFilteredRowModel: getFilteredRowModel(),
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      rowSelection,
+      globalFilter: filtering,
+      ...(usePagination
+        ? { pagination: { pageIndex: page - 1, pageSize } }
+        : {}),
+    },
+    getRowId: (row) => row.id,
+    onSortingChange: setSorting,
+    onGlobalFilterChange: setFiltering,
+  });
+}
