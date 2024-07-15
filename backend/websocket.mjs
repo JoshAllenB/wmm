cb38edb3de35cb470f8889f379af97d194d91dd4
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-07-15 17:16:38 +0800

    cleaned up socket routes.

--- backend/websocket.mjs
+++ backend/websocket.mjs
@@ -1,21 +1,21 @@
 const initWebSocket = (io) => {
   io.on("connection", (socket) => {
-    console.log("A user is connected");
+    console.log(`A user is connected: ${socket.id}`);
 
     socket.on("data-update", (data) => {
       console.log("Data update received", data);
     });
 
-    socket.on("disconnect", () => {
-      console.log("A user is disconnected");
+    socket.on("user-update", (data) => {
+      console.log("User update received:", data);
     });
 
-    setInterval(() => {
-      socket.emit("ping");
-    }, 5000);
+    socket.on("disconnect", (reason) => {
+      console.log(`A user is disconnected: ${socket.id}`, reason);
+    });
 
-    socket.on("ping", () => {
-      socket.emit("pong");
+    socket.on("error", (error) => {
+      console.log("Socket error:", error);
     });
   });
 };
