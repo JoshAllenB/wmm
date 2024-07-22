512aa0496f4a1226336d573fdc1f70feb74cf8dc
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-07-22 17:10:42 +0800

    added custom user timeout

--- client/src/components/UI/Topbar.jsx
+++ client/src/components/UI/Topbar.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { IconButton, useTheme } from "@mui/material";
 import { ColorModeContext, tokens } from "./Theme/theme.utils";
 import AccountCircleSharpIcon from "@mui/icons-material/AccountCircleSharp";
@@ -11,24 +11,40 @@ import {
   DropdownMenuContent,
   DropdownMenuLabel,
   DropdownMenuSeparator,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
+  DropdownMenuPortal,
+  DropdownMenuItem,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
 } from "./ShadCN/dropdown-menu";
 import Logout from "../../utils/UserAuth/logout";
+import { DropdownMenuGroup } from "@radix-ui/react-dropdown-menu";
+import { CircleUser, LogOut, Timer } from "lucide-react";
 
-export default function Topbar({ setIsLoggedIn }) {
+export default function Topbar({ setIsLoggedIn, onInactivityTimeoutChange }) {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
+  const [, setInactiveTimeout] = useState(300); // Default to 5 minutes (300 seconds)
+
+  const handleTimeout = (timeout) => {
+    setInactiveTimeout(timeout);
+    if (typeof onInactivityTimeoutChange === "function") {
+      onInactivityTimeoutChange(timeout);
+    } else {
+      console.error("onInactivityTimeoutChange is not a function");
+    }
+  };
 
   const handleClick = () => {
     colorMode.toggleColorMode();
   };
 
   return (
-    <div
-      style={{ backgroundColor: colors.neutral }}
-      className="w-full flex justify-end"
-    >
+    <div className="w-full flex justify-end">
       <IconButton onClick={handleClick} className="w-[50px]">
         {theme.palette.mode === "dark" ? (
           <LightModeSharpIcon color="neutral" />
@@ -49,10 +65,48 @@ export default function Topbar({ setIsLoggedIn }) {
             <AccountCircleSharpIcon color="neutral" />
           </IconButton>
         </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>Account Settings</DropdownMenuLabel>
+        <DropdownMenuContent
+          className={
+            colors.mode === "dark"
+              ? "bg-slate-200 text-black"
+              : "bg-slate-600 text-white"
+          }
+        >
+          <DropdownMenuLabel className="flex items-center">
+            <CircleUser className="mr-2" />
+            <span>Account Settings</span>
+          </DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <Logout setIsLoggedIn={setIsLoggedIn} />
+          <DropdownMenuGroup>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                <Timer className="mr-2 h-4 w-4" />
+                <span>Inactivity Timeout</span>
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent>
+                  <DropdownMenuRadioGroup>
+                    <DropdownMenuRadioItem onClick={() => handleTimeout(30)}>
+                      30 seconds
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem onClick={() => handleTimeout(300)}>
+                      5 minutes
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem onClick={() => handleTimeout(900)}>
+                      15 minutes
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem onClick={() => handleTimeout(1800)}>
+                      30 minutes
+                    </DropdownMenuRadioItem>
+                  </DropdownMenuRadioGroup>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+            <DropdownMenuItem>
+              <LogOut className="mr-2 h-4 w-4" />
+              <Logout setIsLoggedIn={setIsLoggedIn} />
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
