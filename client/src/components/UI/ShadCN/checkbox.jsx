b3ef3dea4bf7e273d4fc12caf8f941e0fa636666
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-09-16 15:09:59 +0800

    New toast component and hook, minor UI changes

--- client/src/components/UI/ShadCN/checkbox.jsx
+++ client/src/components/UI/ShadCN/checkbox.jsx
@@ -3,57 +3,25 @@
 import * as React from "react";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { Check } from "lucide-react";
-import { useTheme } from "@mui/material";
-import { tokens } from "../Theme/theme.utils";
 
 import { cn } from "@/lib/utils";
 
-const Checkbox = React.forwardRef(({ className, ...props }, ref) => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-
-  const checkboxStyles = {
-    root: {
-      width: "24px",
-      height: "24px",
-      border: `3px solid ${
-        colors.mirage[theme.palette.mode === "dark" ? 500 : 100]
-      }`,
-      outline: "none",
-      outlineOffset: "2px",
-      outlineColor: `${
-        colors.mirage[theme.palette.mode === "dark" ? 300 : 500]
-      }`,
-    },
-    indicator: {
-      backgroundColor: `${
-        colors.mirage[theme.palette.mode === "dark" ? 500 : 300]
-      }`,
-      color: `${colors.white[theme.palette.mode === "dark" ? 400 : 200]}`,
-      width: "100%",
-      height: "100%",
-    },
-  };
-
-  return (
-    <CheckboxPrimitive.Root
-      ref={ref}
-      className={cn(
-        `peer h-4 w-4 shrink-0 rounded-sm disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:text-${colors.white[500]}`,
-        className
-      )}
-      style={checkboxStyles.root}
-      {...props}
+const Checkbox = React.forwardRef(({ className, ...props }, ref) => (
+  <CheckboxPrimitive.Root
+    ref={ref}
+    className={cn(
+      "peer h-[20px] w-[20px] rounded-sm border-2 border-blue-800 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-blue-500 data-[state=checked]:text-slate-50 dark:border-slate-800 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 dark:data-[state=checked]:bg-slate-50 dark:data-[state=checked]:text-slate-900",
+      className,
+    )}
+    {...props}
+  >
+    <CheckboxPrimitive.Indicator
+      className={cn("flex items-center justify-center text-current")}
     >
-      <CheckboxPrimitive.Indicator
-        className={cn("flex items-center justify-center text-current")}
-        style={checkboxStyles.indicator}
-      >
-        <Check className="h-4 w-4" />
-      </CheckboxPrimitive.Indicator>
-    </CheckboxPrimitive.Root>
-  );
-});
+      <Check className="h-4 w-4" />
+    </CheckboxPrimitive.Indicator>
+  </CheckboxPrimitive.Root>
+));
 Checkbox.displayName = CheckboxPrimitive.Root.displayName;
 
 export { Checkbox };
--- client/src/components/UI/ShadCN/tabs.jsx
+++ client/src/components/UI/ShadCN/tabs.jsx
@@ -11,8 +11,8 @@ const TabsList = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md bg-gray-600  p-1 text-foreground",
-      className
+      "inline-flex h-10 items-center justify-center rounded-md bg-blue-600 p-1 text-slate-400 dark:bg-slate-800 dark:text-slate-400",
+      className,
     )}
     {...props}
   />
@@ -23,10 +23,8 @@ const TabsTrigger = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-lg px-5 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-5 focus-visible:ring-ring focus-visible:ring-offset-3 disabled:pointer-events-none disabled:opacity-50",
-      "data-[state=active]:bg-gray-800 data-[state=active]:text-white data-[state=active]:shadow-md rounded-lg",
-      "hover:bg-gray-900",
-      className
+      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-slate-100 data-[state=active]:text-slate-950 data-[state=active]:shadow-sm dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 dark:data-[state=active]:bg-slate-950 dark:data-[state=active]:text-slate-50",
+      className,
     )}
     {...props}
   />
@@ -37,8 +35,8 @@ const TabsContent = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.Content
     ref={ref}
     className={cn(
-      "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
-      className
+      "mt-2 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+      className,
     )}
     {...props}
   />
