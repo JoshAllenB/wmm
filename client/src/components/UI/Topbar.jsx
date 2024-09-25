53d5eed63c1aa773b6f0873a0561efbd18cc4400
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-09-25 15:17:11 +0800

    ui changes

--- client/src/components/UI/Topbar.jsx
+++ client/src/components/UI/Topbar.jsx
@@ -1,11 +1,8 @@
-import { useContext, useState } from "react";
-import { IconButton, useTheme } from "@mui/material";
-import { ColorModeContext, tokens } from "./Theme/theme.utils";
+import { useState } from "react";
+import { IconButton } from "@mui/material";
 import AccountCircleSharpIcon from "@mui/icons-material/AccountCircleSharp";
 import SettingsSharpIcon from "@mui/icons-material/SettingsSharp";
 import NotificationsActiveSharpIcon from "@mui/icons-material/NotificationsActiveSharp";
-import LightModeSharpIcon from "@mui/icons-material/LightModeSharp";
-import DarkModeSharpIcon from "@mui/icons-material/DarkModeSharp";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -25,9 +22,6 @@ import { DropdownMenuGroup } from "@radix-ui/react-dropdown-menu";
 import { CircleUser, LogOut, Timer } from "lucide-react";
 
 export default function Topbar({ setIsLoggedIn, onInactivityTimeoutChange }) {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  const colorMode = useContext(ColorModeContext);
   const [, setInactiveTimeout] = useState(300); // Default to 5 minutes (300 seconds)
 
   const handleTimeout = (timeout) => {
@@ -39,20 +33,8 @@ export default function Topbar({ setIsLoggedIn, onInactivityTimeoutChange }) {
     }
   };
 
-  const handleClick = () => {
-    colorMode.toggleColorMode();
-  };
-
   return (
     <div className="w-full flex justify-end">
-      <IconButton onClick={handleClick} className="w-[50px]">
-        {theme.palette.mode === "dark" ? (
-          <LightModeSharpIcon color="neutral" />
-        ) : (
-          <DarkModeSharpIcon color="neutral" />
-        )}
-      </IconButton>
-
       <IconButton className="w-[50px]">
         <NotificationsActiveSharpIcon color="neutral" />
       </IconButton>
@@ -65,13 +47,7 @@ export default function Topbar({ setIsLoggedIn, onInactivityTimeoutChange }) {
             <AccountCircleSharpIcon color="neutral" />
           </IconButton>
         </DropdownMenuTrigger>
-        <DropdownMenuContent
-          className={
-            colors.mode === "dark"
-              ? "bg-slate-200 text-black"
-              : "bg-slate-600 text-white"
-          }
-        >
+        <DropdownMenuContent>
           <DropdownMenuLabel className="flex items-center">
             <CircleUser className="mr-2" />
             <span>Account Settings</span>
