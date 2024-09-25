04b211720f38530089fad0895d958a7460e8395f
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-09-25 14:30:24 +0800

    changed password hashing

--- backend/models/userControl/users.mjs
+++ backend/models/userControl/users.mjs
@@ -1,13 +1,7 @@
-// user.mjs
 import mongoose from "mongoose";
-import { v4 as uuidv4 } from "uuid";
-import crypto from "crypto";
+import bcrypt from "bcrypt";
 import dotenv from "dotenv";
-import { Role } from "./role.mjs";
-
-dotenv.config();
-console.log("users.mjs", process.env.MONGODB_URI_USER);
-const userConnection = mongoose.createConnection(process.env.MONGODB_URI_USER);
+import dbConnection from "../dbConnect.mjs";
 
 const UsersSchema = new mongoose.Schema(
   {
@@ -17,16 +11,14 @@ const UsersSchema = new mongoose.Schema(
       trim: true,
       unique: true,
     },
-    encrypted_password: {
+    password: {
       type: String,
       required: true,
     },
     role: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Role",
-      required: false,
+      ref: "roles",
     },
-    salt: String,
     lastLoginAt: Date,
     status: {
       type: String,
@@ -37,34 +29,22 @@ const UsersSchema = new mongoose.Schema(
   { timestamps: true, collection: "users" },
 );
 
-UsersSchema.virtual("password")
-  .set(function (password) {
-    this._password = password;
-    this.salt = uuidv4();
-    this.encrypted_password = this.securePassword(password);
-  })
-  .get(function () {
-    return this._password;
-  });
+UsersSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-UsersSchema.methods = {
-  authenticate(plainpassword) {
-    return this.securePassword(plainpassword) === this.encrypted_password;
-  },
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
-  securePassword(plainpassword) {
-    if (!plainpassword) return "";
-    try {
-      return crypto
-        .createHmac("sha256", this.salt)
-        .update(plainpassword)
-        .digest("hex");
-    } catch (err) {
-      return "error in hashing the password";
-    }
-  },
+UsersSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
 };
 
-const UserModel = userConnection.model("users", UsersSchema);
+const UserModel = dbConnection.model("users", UsersSchema);
 
 export default UserModel;
