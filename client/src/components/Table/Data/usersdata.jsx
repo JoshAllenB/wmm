2bdff3963309eb06ea3164acec6d8127c3bbb28a
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-09-25 15:13:52 +0800

    minor user data fetch changes, includes role and permisison in the fetch

--- client/src/components/Table/Data/usersdata.jsx
+++ client/src/components/Table/Data/usersdata.jsx
@@ -1,21 +1,28 @@
 import axios from "axios";
 
-export const fetchUsers = async (setUsersData) => {
+export const fetchUsers = async () => {
   try {
     const response = await axios.get("http://localhost:3001/users", {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
       },
     });
+    const { users, currentUser } = response.data;
 
-    const users = response.data.map((user) => ({
+    const formattedUsers = users.map((user) => ({
       ...user,
-      status: user.status.status || user.status,
+      status: user.status || "Inactive",
+      role: user.role, // Keep role without permissions
     }));
 
-    setUsersData(users);
-    return users;
+    const formattedCurrentUser = {
+      ...currentUser,
+      role: currentUser.role, // Keep role without permissions
+    };
+
+    return [formattedUsers, formattedCurrentUser]; // Return both values as an array
   } catch (err) {
     console.error("Error fetching user data:", err);
+    return { users: [], currentUser: null };
   }
 };
