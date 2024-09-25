297cd351260f3ffd0a2de5a8ae551126139be781
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-09-25 14:35:35 +0800

    added an api for Philippine addresses, might be temporary

--- backend/server.mjs
+++ backend/server.mjs
@@ -7,12 +7,14 @@ import cors from "cors";
 import { fileURLToPath } from "url";
 import path from "path";
 import userAuthRouter from "./userAuth/userAuth.mjs";
-import initWebSocket from "./websocket.mjs"; // New import for WebSocket logic
-
+import initWebSocket from "./websocket.mjs";
+import UserModel from "./models/userControl/users.mjs";
+import { Role, Permission } from "./models/userControl/role.mjs";
 import userRoutes from "./middleware/users/Users.mjs";
 import clientsRoutes from "./middleware/clients/Clients.mjs";
 import hrgRoutes from "./middleware/hrg/Hrg.mjs";
 import wmmRoutes from "./middleware/wmm/wmm.mjs";
+import axios from "axios";
 
 dotenv.config();
 
@@ -53,6 +55,28 @@ app.use("/users", attachIO, userRoutes);
 app.use("/clients", attachIO, clientsRoutes);
 app.use("/hrg", attachIO, hrgRoutes);
 app.use("/wmm", attachIO, wmmRoutes);
+
+const PSGC_API_BASE_URL = "https://psgc.gitlab.io/api";
+
+app.get("/api/:endpoint(*)", async (req, res) => {
+  try {
+    const { endpoint } = req.params;
+
+    // Build the full URL (append '.json' if necessary)
+    const finalEndpoint = endpoint.endsWith(".json")
+      ? endpoint
+      : `${endpoint}.json`;
+    const fullUrl = `${PSGC_API_BASE_URL}/${finalEndpoint}`;
+
+    // Make the request to the PSGC API
+    const response = await axios.get(fullUrl);
+
+    res.json(response.data); // Send the data to the frontend
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
+});
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
