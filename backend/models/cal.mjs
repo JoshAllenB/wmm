ae041ad5935ac0a06a3b390751c5d6149f084cc9
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-10-20 20:45:31 +0800

    Add cal model with MongoDB connection and schema

---
+++ backend/models/cal.mjs
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const calConnection = mongoose.createConnection(process.env.MONGODB_URI_CLIENT);
+
+const CalSchema = new mongoose.Schema({
+  id: Number,
+  clientid: Number,
+  recvdate: String,
+  caltype: String,
+  calqty: Number,
+  calamt: Number,
+  paymtref: String,
+  paymtamt: Number,
+  paymtform: String,
+  paymtdate: String,
+  adddate: String,
+  adduser: String,
+}, {
+  versionKey: false,
+  collection: "cal",
+});
+
+const CalModel = calConnection.model("cal", CalSchema);
+
+export default CalModel;
+  
