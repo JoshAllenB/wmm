55c321a67000a9e9c1b79c2855c004fdcadebc20
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-10-20 21:00:03 +0800

    Updated pagination logic and ui

--- client/src/components/Table/Features/Pagination.jsx
+++ client/src/components/Table/Features/Pagination.jsx
@@ -1,31 +1,56 @@
 import { Button } from "../../UI/ShadCN/button";
 
 export const PaginationComponent = ({
-  table,
-  page,
   totalPages,
   handlePreviousPage,
   handleNextPage,
+  pageSize,
+  setPageSize,
+  page,
+  setPage,
 }) => {
+  const handlePageSizeChange = (e) => {
+    setPageSize(Number(e.target.value));
+    setPage(1);
+  };
   return (
-    <div className="mt-4 flex items-center gap-2">
-      <Button
-        disabled={!table.getCanPreviousPage()}
-        onClick={handlePreviousPage}
-        className="text-white bg-blue-500"
-      >
-        Previous
-      </Button>
-      <span className="">
+    <div className="flex items-center justify-between px-2 mt-2">
+      <div className="flex-1 text-sm text-muted-foreground">
         Page {page} of {totalPages}
-      </span>
-      <Button
-        disabled={!table.getCanNextPage()}
-        onClick={handleNextPage}
-        className="text-white bg-blue-500"
-      >
-        Next
-      </Button>
+      </div>
+      <div className="flex items-center space-x-6 lg:space-x-8">
+        <div className="flex items-center space-x-2">
+          <p className="text-sm font-medium">Rows per page</p>
+          <select
+            className="h-8 w-[70px] rounded-md border border-input bg-transparent"
+            value={pageSize}
+            onChange={handlePageSizeChange}
+          >
+            {[10, 20, 30, 50, 100].map((size) => (
+              <option key={size} value={size}>
+                {size}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="flex items-center space-x-2">
+          <Button
+            className="px-2 py-1 border rounded bg-blue-500 hover:bg-blue-600 text-white"
+            onClick={handlePreviousPage}
+            disabled={page === 1}
+          >
+            Previous
+          </Button>
+          <Button
+            className="px-2 py-1 border rounded bg-blue-500 hover:bg-blue-600 text-white"
+            onClick={handleNextPage}
+            disabled={page === totalPages}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
