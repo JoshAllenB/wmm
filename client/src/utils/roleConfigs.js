9fde7e8117e35f69631ca8550297a541f908f96f
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-10-20 21:03:52 +0800

    Add roleConfigs.js with group fields for different roles

---
+++ client/src/utils/roleConfigs.js
@@ -0,0 +1,45 @@
+export const roleConfigs = {
+  WMM: {
+    groupFields: {
+      subsdate: "Subscription Date",
+      enddate: "End Date",
+      renewdate: "Renewal Date",
+      subsyear: "Subscription Year",
+      copies: "Copies",
+    },
+  },
+  HRG: {
+    groupFields: {
+      recvdate: "Received Date",
+      renewdate: "Renewal Date",
+      campaigndate: "Campaign Date",
+      paymtref: "Payment Reference",
+      paymtamt: "Payment Amount",
+      unsubscribe: "Unsubscribe",
+    },
+  },
+  FOM: {
+    groupFields: {
+      recvdate: "Received Date",
+      remarks: "Remarks",
+      paymtamt: "Payment Amount",
+      unsubscribe: "Unsubscribe",
+      adddate: "Add Date",
+      adduser: "Add User",
+    },
+  },
+  CAL: {
+    groupFields: {
+      recvdate: "Received Date",
+      caltype: "Calendar Type",
+      calqty: "Calendar Quantity",
+      calamt: "Calendar Amount",
+      paymtref: "Payment Reference",
+      paymtamt: "Payment Amount",
+      paymtform: "Payment Form",
+      paymtdate: "Payment Date",
+      adddate: "Add Date",
+      adduser: "Add User",
+    },
+  },
+};
