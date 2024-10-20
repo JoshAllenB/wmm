1094e2544da228a0a23ab5a87142d360b0c0643d
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-10-20 20:34:00 +0800

    added area routes removed the PSGC api logic

--- backend/server.mjs
+++ backend/server.mjs
@@ -8,14 +8,12 @@ import { fileURLToPath } from "url";
 import path from "path";
 import userAuthRouter from "./userAuth/userAuth.mjs";
 import initWebSocket from "./websocket.mjs";
-import UserModel from "./models/userControl/users.mjs";
-import { Role, Permission } from "./models/userControl/role.mjs";
 import userRoutes from "./middleware/users/Users.mjs";
 import clientsRoutes from "./middleware/clients/Clients.mjs";
 import hrgRoutes from "./middleware/hrg/Hrg.mjs";
 import wmmRoutes from "./middleware/wmm/wmm.mjs";
 import roleRoutes from "./middleware/users/roleManagement.mjs";
-import axios from "axios";
+import areaRoutes from "./middleware/areaApiRoute.mjs";
 
 dotenv.config();
 
@@ -29,7 +27,7 @@ app.use(
     origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+  })
 );
 
 const server = http.createServer(app);
@@ -57,28 +55,7 @@ app.use("/clients", attachIO, clientsRoutes);
 app.use("/hrg", attachIO, hrgRoutes);
 app.use("/wmm", attachIO, wmmRoutes);
 app.use("/roles", attachIO, roleRoutes);
-
-const PSGC_API_BASE_URL = "https://psgc.gitlab.io/api";
-
-app.get("/api/:endpoint(*)", async (req, res) => {
-  try {
-    const { endpoint } = req.params;
-
-    // Build the full URL (append '.json' if necessary)
-    const finalEndpoint = endpoint.endsWith(".json")
-      ? endpoint
-      : `${endpoint}.json`;
-    const fullUrl = `${PSGC_API_BASE_URL}/${finalEndpoint}`;
-
-    // Make the request to the PSGC API
-    const response = await axios.get(fullUrl);
-
-    res.json(response.data); // Send the data to the frontend
-  } catch (error) {
-    console.error("Error fetching data:", error.message);
-    res.status(500).json({ error: "An error occurred while fetching data" });
-  }
-});
+app.use("/areas", attachIO, areaRoutes);
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
