1d3d249acb2e5942234f61270c0a7566f93cb9c8
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-10-24 10:40:49 +0800

    Changed db conneciton dbConnect.mjs

--- backend/models/dbConnect.mjs
+++ backend/models/dbConnect.mjs
@@ -4,7 +4,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const dbConnection = mongoose.createConnection(process.env.MONGODB_URI_USER);
+const dbConnection = mongoose.createConnection(process.env.MONGODB_URI, {
+  dbName: process.env.DB_NAME_USER,
+});
 
 dbConnection.once("open", () => {
   console.log("Connection to MongoDB established");
