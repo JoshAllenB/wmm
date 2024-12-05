512edd9dfed198a28961161b0a5338ad0401252d
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-12-05 15:10:41 +0800

    refactor: better error and data handling

--- client/src/utils/Websocket/useSocket.js
+++ client/src/utils/Websocket/useSocket.js
@@ -2,5 +2,19 @@ import { useContext } from "react";
 import { SocketContext } from "./SocketContext";
 
 export const useSocket = () => {
-  return useContext(SocketContext);
+  const context = useContext(SocketContext);
+  
+  if (!context) {
+    throw new Error('useSocket must be used within a SocketProvider');
+  }
+
+  const { socket, socketData } = context;
+
+  return {
+    socket,
+    socketData,
+    emit: (event, data) => socket.emit(event, data),
+    subscribe: (event, handler) => socket.subscribe(event, handler),
+    unsubscribe: (event, handler) => socket.unsubscribe(event, handler)
+  };
 };
