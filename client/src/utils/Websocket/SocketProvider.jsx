e3891737ae37180deb9a658b8d855a8722b4abe2
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-12-06 19:07:12 +0800

    minor websocket logic changes

--- client/src/utils/Websocket/SocketProvider.jsx
+++ client/src/utils/Websocket/SocketProvider.jsx
@@ -5,26 +5,25 @@ import { webSocketService } from "../../services/WebSocketService";
 export const SocketProvider = ({ children }) => {
   const [socketData, setSocketData] = useState(null);
 
-  const handleSocketData = useCallback((data) => {
-    setSocketData(data);
-  }, []);
-
   useEffect(() => {
     webSocketService.connect();
 
-    // Subscribe to events
-    const events = ['data-update', 'hrg-update', 'user-update'];
-    events.forEach(event => {
+    const handleSocketData = (data) => {
+      setSocketData({ ...data });
+    };
+
+    const events = ["data-update", "hrg-update", "user-update"];
+    events.forEach((event) => {
       webSocketService.subscribe(event, handleSocketData);
     });
 
     return () => {
-      events.forEach(event => {
+      events.forEach((event) => {
         webSocketService.unsubscribe(event, handleSocketData);
       });
       webSocketService.disconnect();
     };
-  }, [handleSocketData]);
+  }, []);
 
   return (
     <SocketContext.Provider value={{ socket: webSocketService, socketData }}>
