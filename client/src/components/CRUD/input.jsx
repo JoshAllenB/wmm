743f2d02343c3f31deac0851e7c5d67e9a0f1599
JoshAllenB <batayolajoshuaallen@gmail.com>
2025-02-02 16:19:27 +0800

    added placeholder hook for inputfield

--- client/src/components/CRUD/input.jsx
+++ client/src/components/CRUD/input.jsx
@@ -1,4 +1,11 @@
-const InputField = ({ label, name, value, onChange, type = "text" }) => (
+const InputField = ({
+  label,
+  name,
+  value,
+  onChange,
+  type = "text",
+  placeholder = "",
+}) => (
   <div className="flex flex-col mb-4">
     <label
       htmlFor={name}
@@ -13,6 +20,7 @@ const InputField = ({ label, name, value, onChange, type = "text" }) => (
       name={name}
       value={value || ""}
       onChange={onChange}
+      placeholder={placeholder}
       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-gray-300 placeholder:text-gray-300 focus:ring-3 p-3"
     />
   </div>
