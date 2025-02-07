722905a38c97a3b7599cfe896071b9ab7a81c8ff
JoshAllenB <batayolajoshuaallen@gmail.com>
2025-02-07 19:48:01 +0800

    Added check for multirow selection

--- client/src/components/Table/TableLogic.js
+++ client/src/components/Table/TableLogic.js
@@ -64,7 +64,7 @@ export function useTableLogic(
     onSortingChange: setSorting,
     onGlobalFilterChange: setFiltering,
     enableRowSelection: true,
-    enableMultiRowSelection: false,
+    enableMultiRowSelection: true,
   });
 
   return table;
