58a276748cbee97d29846571d460d0402fcaa39f
JoshAllenB <batayolajoshuaallen@gmail.com>
2025-06-01 21:20:05 +0800

    test logic and packages temporary in git

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,4 +10,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+    css: true,
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+  },
 });
