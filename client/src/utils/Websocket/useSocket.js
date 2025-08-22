2163e8092e1632f736dbbc1120047341fefba3d8
JoshAllenB <batayolajoshuaallen@gmail.com>
2025-08-22 14:27:48 +0800

    better websocket connection and reconnect

--- client/src/utils/Websocket/useSocket.js
+++ client/src/utils/Websocket/useSocket.js
@@ -8,13 +8,24 @@ export const useSocket = () => {
     throw new Error('useSocket must be used within a SocketProvider');
   }
 
-  const { socket, socketData } = context;
+  const { socket, socketData, connected, connectionStatus, updateConnectionStatus } = context;
 
   return {
     socket,
     socketData,
+    connected,
+    connectionStatus,
+    updateConnectionStatus,
     emit: (event, data) => socket.emit(event, data),
     subscribe: (event, handler) => socket.subscribe(event, handler),
-    unsubscribe: (event, handler) => socket.unsubscribe(event, handler)
+    unsubscribe: (event, handler) => socket.unsubscribe(event, handler),
+    reconnect: () => socket.reconnect(),
+    getConnectionStatus: () => socket.getConnectionStatus(),
+    // Helper methods for connection management
+    isConnected: () => connected,
+    isConnecting: () => connectionStatus.connectionState === 'connecting',
+    isReconnecting: () => connectionStatus.connectionState === 'reconnecting',
+    getReconnectAttempts: () => connectionStatus.reconnectAttempts,
+    getMaxReconnectAttempts: () => connectionStatus.maxReconnectAttempts
   };
 };
